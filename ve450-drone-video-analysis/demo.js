8c499fcc3b3beb3d22934403fcce311cbbf8d2e3
jiyuchennn <112918020+jiyuchennn@users.noreply.github.com>
2023-07-18 21:37:32 +0800

    add to demo.js

--- ve450-drone-video-analysis/demo.js
+++ ve450-drone-video-analysis/demo.js
@@ -1,1 +1,21 @@
+// url of where to download the video from server
 const url = ""
+
+<button onclick="download_video()">refresh</button>
+
+function download_video(url, element_id){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'blob';
+    xhr.onload = function() {
+      var urlCreator = window.URL || window.webkitURL;
+      var video_url = urlCreator.createObjectURL(this.response);
+      element.href = video_url;
+    };
+    xhr.onerror = err => {
+      alert('Failed to download picture');
+    };
+    xhr.send();
+}
+
+
