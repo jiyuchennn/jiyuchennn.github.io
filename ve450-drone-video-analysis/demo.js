01d642d3700984d55117cae47d48d426a7b9172e
jerrycmy <jerrycmy@umich.edu>
2023-07-21 18:20:08 +0800

    Fix window time not contiguous bug

--- ve450-drone-video-analysis/demo.js
+++ ve450-drone-video-analysis/demo.js
@@ -2,6 +2,7 @@
 let server_url = "http://49.235.101.153:5000/video/";
 let window_size = 5;
 let fetch_interval = 1
+let move_detech_interval = 0.1
 
 let video_component = document.getElementById("live-video");
 let start_time = get_time();
@@ -12,30 +13,19 @@ for (var i = 0; i < window_size; i++) {
     video_window.push({time : start_time - window_size + i, url : null});
 }
 
-// setInterval(swap_video, 1000);
-// function swap_video()
-// {
-//     let time = get_time();
-//     console.log(time);
-//     if (time % 2 == 0) {
-//         video_component.src = "files/original.mp4";
-//     }
-//     else {
-//         video_component.src = "files/YOLO.mp4";
-//     }
-// }
-
 setInterval(try_fetch_video, fetch_interval * 1000)
-setInterval(try_move_window, 1000)
+setInterval(try_move_window, move_detech_interval * 1000)
 
 function try_move_window()
 {
     let time = get_time();
     while (video_window[0].time < time - window_size + 1) {
         let video_to_play = video_window.shift();
-        video_window.push({time : time, url : null});
+        video_window.push({time : video_window.at(-1).time + 1, url : null});
+        console.log(`New window: ${video_window}`)
 
-        if (video_to_play.url !== null) {
+        if (video_to_play.url !== null)
+        {
             if (curr_video_url !== null) {
                 URL.revokeObjectURL(curr_video_url);
             }
@@ -66,16 +56,3 @@ function try_fetch_video()
 }
 
 function get_time() { return Math.floor(Date.now() / 1000); }
-// function download_video(url, element_id)
-// {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open('GET', url, true);
-//     xhr.responseType = 'blob';
-//     xhr.onload = function() {
-//         var urlCreator = window.URL || window.webkitURL;
-//         var video_url = urlCreator.createObjectURL(this.response);
-//         element.href = video_url;
-//     };
-//     xhr.onerror = err => { alert('Failed to download picture'); };
-//     xhr.send();
-// }
